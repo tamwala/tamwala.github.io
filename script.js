0927862e72880470dfc1e79dbfa4bff5c165f8f4
Tamara Wala <tamwala@gmail.com>
2022-10-08 14:08:45 +0200

    tweaking script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-tawal.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://kodilla-tasks-tawal.herokuapp.com/v1/tasks';
+  const trelloApiRoot = 'https://kodilla-tasks-tawal.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
